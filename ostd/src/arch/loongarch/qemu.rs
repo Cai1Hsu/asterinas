bc8096c89585799f116c969be5599db37b3d9cf2
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-11 18:56:24 +0800

    feat(ostd/la64): add QEMU exit functionality

---
+++ ostd/src/arch/loongarch/qemu.rs
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MPL-2.0
+
+//! Providing the ability to exit QEMU and return a value as debug result.
+
+use crate::arch::device::mem_port::{MemPort, WriteOnlyAccess};
+
+/// The exit code of QEMU.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum QemuExitCode {
+    /// The code that indicates a successful exit.
+    Success,
+    /// The code that indicates a failed exit.
+    Failed,
+}
+
+/// Exit QEMU with the given exit code.
+pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
+    log::debug!("exit qemu with exit code {exit_code:?}");
+    let port = unsafe { MemPort::<u8, WriteOnlyAccess>::new(0x100e001c) };
+
+    port.write(0x34);
+
+    unreachable!("qemu did not exit");
+}
