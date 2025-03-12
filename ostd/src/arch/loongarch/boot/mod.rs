d25b09c861d18fb8b84090016556428c3e7f4da9
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-12 20:36:13 +0800

    feat(ostd/la64): parse device and initialize EARLY_INFO

--- ostd/src/arch/loongarch/boot/mod.rs
+++ ostd/src/arch/loongarch/boot/mod.rs
@@ -6,13 +6,85 @@ pub mod smp;
 
 use core::arch::global_asm;
 
-use crate::boot::call_ostd_main;
+use fdt::Fdt;
+use spin::Once;
+
+use crate::{
+    boot::{
+        memory_region::{MemoryRegion, MemoryRegionArray, MemoryRegionType},
+        BootloaderAcpiArg, BootloaderFramebufferArg,
+    },
+    mm::paddr_to_vaddr,
+};
 
 global_asm!(include_str!("boot.S"));
 
-#[no_mangle]
-pub extern "C" fn loongarch_boot(_cpu_id: usize, _device_tree_paddr: usize) -> ! {
+/// The Flattened Device Tree of the platform.
+pub static DEVICE_TREE: Once<Fdt> = Once::new();
+
+fn parse_bootloader_name() -> &'static str {
+    "unknown"
+}
+
+fn parse_kernel_commandline() -> &'static str {
+    DEVICE_TREE.get().unwrap().chosen().bootargs().unwrap_or("")
+}
+
+fn parse_initramfs() -> Option<&'static [u8]> {
     // TODO
+    None
+}
+
+fn parse_acpi_arg() -> BootloaderAcpiArg {
+    BootloaderAcpiArg::NotProvided
+}
+
+fn parse_framebuffer_info() -> Option<BootloaderFramebufferArg> {
+    None
+}
+
+fn parse_memory_regions() -> MemoryRegionArray {
+    let mut regions = MemoryRegionArray::new();
+
+    // add memory region
+    for region in DEVICE_TREE.get().unwrap().memory().regions() {
+        let region_size = region.size.unwrap_or(0);
+        if region_size > 0 {
+            regions.push(MemoryRegion::new(
+                region.starting_address as usize,
+                region_size,
+                MemoryRegionType::Usable,
+            ));
+        }
+    }
+
+    // TODO: add reserved region.
+
+    // add the kernel region.
+    regions.push(MemoryRegion::kernel());
+
+    // TODO: add initramfs region.
+
+    regions.into_non_overlapping()
+}
+
+#[no_mangle]
+pub extern "C" fn loongarch_boot(_cpu_id: usize, device_tree_paddr: usize) -> ! {
+    // TODO
+    let device_tree_ptr = paddr_to_vaddr(device_tree_paddr) as *const u8;
+    let fdt = unsafe { Fdt::from_ptr(device_tree_ptr).unwrap() };
+    DEVICE_TREE.call_once(|| fdt);
+
+    use crate::boot::{call_ostd_main, EarlyBootInfo, EARLY_INFO};
+
+    EARLY_INFO.call_once(|| EarlyBootInfo {
+        bootloader_name: parse_bootloader_name(),
+        kernel_cmdline: parse_kernel_commandline(),
+        initramfs: parse_initramfs(),
+        acpi_arg: parse_acpi_arg(),
+        framebuffer_arg: parse_framebuffer_info(),
+        memory_regions: parse_memory_regions(),
+    });
 
     call_ostd_main();
 }
