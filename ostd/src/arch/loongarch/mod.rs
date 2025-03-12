7c1bfd162e2888bcc8c3c20d1bbf1aa009a2e9e9
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-12 19:56:20 +0800

    feat(ostd/la64): add no op implementation CVM guest initialization

--- ostd/src/arch/loongarch/mod.rs
+++ ostd/src/arch/loongarch/mod.rs
@@ -2,6 +2,11 @@ pub mod device;
 pub mod qemu;
 pub mod serial;
 
+#[cfg(feature = "cvm_guest")]
+pub(crate) fn init_cvm_guest() {
+    // Unimplemented, no-op
+}
+
 pub(crate) fn enable_cpu_features() {
     // enable float point
     loongArch64::register::euen::set_fpe(true);
